2dd364395636385d45749ac4d1b02f5f397d912b
Ricard Hw <ric4rd11@gmail.com>
2023-10-29 13:04:16 -0600

    5bct

--- 5_Arbol_Busqueda_Binaria/bst.h
+++ 5_Arbol_Busqueda_Binaria/bst.h
@@ -22,6 +22,8 @@ class Node{
         T value;
 	    Node *left, *right;
 
+        Node<T>* succesor();
+
     public:
         //Constructors
         Node(T);
@@ -29,9 +31,18 @@ class Node{
         //Methods
         void add(T);
         bool find(T);
+        void remove(T);
+        void removeChilds();
+		int height() const;
+		bool ancestors(stringstream &, T);
+		int whatlevelamI(T data);
 
         void inorder(std::stringstream&) const;
 	    void preorder(std::stringstream&) const;
+		void postorder(std::stringstream&) const;
+		void levelorder(std::stringstream&) const;
+
+        friend class BST<T>;
 
 };
 
@@ -68,6 +79,150 @@ bool Node<T>::find(T val) {
 	} else if (val > value) {
 		return (right != 0 && right->find(val));
 	}
+	return false;
+}
+
+template <class T>
+Node<T>* Node<T>::succesor() {
+	Node<T> *le, *ri;
+
+	le = left;
+	ri = right;
+
+	if (left == 0) {
+		if (right != 0) {
+			right = 0;
+		}
+		return ri;
+	}
+
+	if (left->right == 0) {
+		left = left->left;
+		le->left = 0;
+		return le;
+	}
+
+	Node<T> *parent, *child;
+	parent = left;
+	child = left->right;
+	while (child->right != 0) {
+		parent = child;
+		child = child->right;
+	}
+	parent->right = child->left;
+	child->left = 0;
+	return child;
+}
+
+template <class T>
+void Node<T>::remove(T val) {
+	Node<T> * succ, *old;
+
+	if (val < value) {
+		if (left != 0) {
+			if (left->value == val) {
+				old = left;
+				succ = left->succesor();
+				if (succ != 0) {
+					succ->left = old->left;
+					succ->right = old->right;
+				}
+				left = succ;
+				delete old;
+			} else {
+				left->remove(val);
+			}
+		}
+	} else if (val > value) {
+		if (right != 0) {
+			if (right->value == val) {
+				old = right;
+				succ = right->succesor();
+				if (succ != 0) {
+					succ->left = old->left;
+					succ->right = old->right;
+				}
+				right = succ;
+				delete old;
+			} else {
+				right->remove(val);
+			}
+		}
+	}
+}
+
+template <class T>
+void Node<T>::removeChilds() {
+	if (left != 0) {
+		left->removeChilds();
+		delete left;
+		left = 0;
+	}
+	if (right != 0) {
+		right->removeChilds();
+		delete right;
+		right = 0;
+	}
+}
+
+template <class T>
+int Node<T>::height() const {
+	int l = 0, r = 0;
+	if (left != 0) {
+		l = left -> height();
+	}
+	if (right != 0) {
+		r = right -> height();
+	}
+	if (l == 0 && r == 0) {
+		return 1;
+	}
+	if (l > r) {
+		return l + 1;
+	}
+	else {
+		return r + 1;
+	}
+}
+
+template <class T>
+bool Node<T>::ancestors(stringstream &aux, T data) {
+	if (data < value) {
+		if (aux.tellp() != 1) {
+			aux << " ";
+		}
+		aux << value;
+		return (left -> ancestors(aux, data) && left != 0);
+	}
+	else if (data == value) {
+		return true;
+	}
+	else if (data > value) {
+		if (aux.tellp() != 1) {
+			aux << " ";
+		}
+		aux << value;
+		return (right -> ancestors(aux, data) && right != 0);
+	}
+	return false;
+}
+
+template <class T>
+int Node<T>::whatlevelamI(T data) {
+	if (data < value) {
+		if (left != 0) {
+			return left -> whatlevelamI(data) + 1;
+		}
+	}
+	else if (data == value) {
+		return 1;
+	}
+	else if (data > value) {
+		if (right != 0) {
+			return right -> whatlevelamI(data) + 1;
+		}
+	}
+	return -1;
 }
 
 template <class T>
@@ -97,6 +252,32 @@ void Node<T>::preorder(std::stringstream &aux) const {
 	}
 }
 
+template <class T>
+void Node<T>::postorder(std::stringstream &aux) const {
+	if (left != 0) {
+		left->postorder(aux);
+		aux << " ";
+	}
+	if (right != 0) {
+		right->postorder(aux);
+		aux << " ";
+	}
+	aux << value;
+}
+
+template <class T>
+void Node<T>::levelorder(std::stringstream &aux) const {
+	aux << value;
+	if (left != 0) {
+		aux << " ";
+		left -> levelorder(aux);
+	}
+	if (right != 0) {
+		aux << " ";
+		right -> levelorder(aux);
+	}
+}
+
 template<class T>
 class BST{
     private:
@@ -111,9 +292,12 @@ class BST{
         bool empty() const;
         void add(T);
         bool find(T) const;
+        void removeAll();
        
         std::string inorder() const;
 	    std::string preorder() const;
+		std::string postorder() const;
+		std::string levelorder() const;
         string visit() const;
         int height() const;
         string ancestors(T) const;
@@ -156,6 +340,15 @@ bool BST<T>::find(T val) const {
 }
 
 template <class T>
+void BST<T>::removeAll() {
+	if (root != 0) {
+		root->removeChilds();
+	}
+	delete root;
+	root = 0;
+}
+
+template <class T>
 std::string BST<T>::inorder() const {
 	std::stringstream aux;
 
@@ -179,6 +372,29 @@ std::string BST<T>::preorder() const {
 	return aux.str();
 }
 
+template <class T>
+std::string BST<T>::postorder() const {
+	std::stringstream aux;
+
+	aux << "[";
+	if (!empty()) {
+		root->postorder(aux);
+	}
+	aux << "]";
+	return aux.str();
+}
+
+template <class T>
+std::string BST<T>::levelorder() const {
+    stringstream aux;
+    aux << "[";
+    if (!empty()) {
+        root -> levelorder(aux);
+    }
+    aux << "]";
+    return aux.str();
+}
+
 template<class T>
 string BST<T>::visit() const{
     std::stringstream aux;
@@ -186,25 +402,38 @@ string BST<T>::visit() const{
     aux << preorder();
     aux << "\n";
     aux << inorder();
+	aux << "\n";
+	aux << postorder();
+	aux << "\n";
+	aux << levelorder();
 
-    return aux
+    return aux.str();
 }
 
 template <class T>
-int BST<T>::height() const{
-
-    return 0;
+int BST<T>::height() const {
+	int h = root -> height();
+	return h;
 }
 
 template <class T>
-string BST<T>::ancestors(T) const{
-    
-    return 0;
+string BST<T>::ancestors(T data) const{
+    stringstream aux;
+	aux << "[";
+	if (root -> find(data)) {
+		root -> ancestors(aux, data);		
+	}
+	aux << "]";
+	return aux.str();
 }
 
 template <class T>
-int BST<T>::whatlevelamI(T) const{
-
-    return 0;
+int BST<T>::whatlevelamI(T data) const{
+	int nivel;
+	if (root) {
+		nivel = root -> whatlevelamI(data);
+	}
+	if (nivel < 0) { return -1;}
+	else { return nivel;}
 }
 #endif /* BST_H_ */
